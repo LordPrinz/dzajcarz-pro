e06fb9c8d9855006f2add35e9aa41f9184dca920
LordPrinz <plazinski2902@gmail.com>
2022-06-12 17:20:38 +0200

    Add collector function

---
+++ src/commands/fun/collector.ts
@@ -0,0 +1,94 @@
+import { ICommand } from "wokcommands";
+import generateInvitation from "../../utilities/discord/generateInvitation";
+import getRandom from "../../utilities/getRandom";
+import { sleep } from "../../utilities/oneLiners";
+
+const collector = {
+	category: "Fun",
+	description: "Execution time",
+
+	testOnly: true,
+
+	callback: ({ message, channel, member }) => {
+		message?.reply("Podbij łapę jeśli chcesz bana");
+		message?.react("👍");
+
+		let hasBeenBanned = false;
+
+		const time = 1000 * 60 * 60 * 24;
+
+		const collector = message?.createReactionCollector({
+			time,
+		});
+
+		collector.on("collect", async (reaction, usr) => {
+			const userId = usr.id;
+
+			const userToBan = await reaction.message.guild?.members.fetch(userId);
+			const message = reaction?.message;
+			if (!userToBan?.bannable) {
+				if (userToBan?.id === "935890396419096706") {
+					return;
+				}
+
+				message?.reply("Nie mogę tego uczynić Panie...");
+				return;
+			}
+
+			message?.reply(`Jak sobie życzysz <@${userId}>`);
+
+			await sleep(3000);
+
+			if (userId === member?.user?.id) {
+				hasBeenBanned = true;
+			}
+			console.log(`BAN ${usr?.username}`);
+
+			message?.reply("No to elo xd");
+
+			userToBan
+				?.ban({
+					reason: "Sam chciał Bana :)",
+				})
+				.catch((err) => console.error(err));
+		});
+
+		collector.on("end", async () => {
+			if (hasBeenBanned) {
+				return;
+			}
+
+			if (!member.kickable) {
+				return;
+			}
+
+			await sleep(3000);
+
+			message?.reply("No cóż...");
+
+			const isLucky = getRandom(0, 4) === 0;
+
+			if (isLucky) {
+				await sleep(3000);
+				message?.reply("Masz szczęście :D");
+				return;
+			}
+
+			const invitation = await generateInvitation(channel);
+
+			await sleep(3000);
+
+			message?.reply("Chyba wylatujsz");
+
+			await sleep(3000);
+
+			channel?.send("No to elo xd");
+
+			await member?.send(`https://discord.gg/${invitation.code}`).catch(() => {});
+			console.log(`KICK ${member?.user?.username}`);
+			message?.member?.kick().catch(() => {});
+		});
+	},
+} as ICommand;
+
+export default collector;
--- src/utilities/oneLiners.ts
+++ src/utilities/oneLiners.ts
@@ -1,3 +1,7 @@
 export const isDivisible = (number: number, divider: number): boolean => {
 	return number % divider === 0;
 };
+
+export const sleep = (ms: number) => {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+};
