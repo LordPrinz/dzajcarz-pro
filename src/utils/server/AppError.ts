4ae3e7562ad7d2f4c4118427b75530ad051d8fa5
LordPrinz <plazinski2902@gmail.com>
2023-06-05 18:42:23 +0200

    Create ApiFeatures

---
+++ src/utils/server/AppError.ts
@@ -0,0 +1,22 @@
+import Logger from "../debug/Logger";
+
+class AppError extends Error {
+	statusCode: number;
+	status: "fail" | "error";
+	isOperational: boolean;
+	constructor(message: string, statusCode: number) {
+		const logger = new Logger();
+
+		super(message);
+
+		this.statusCode = statusCode;
+		this.status = statusCode.toString().startsWith("4") ? "fail" : "error";
+		this.isOperational = true;
+
+		Error.captureStackTrace(this, this.constructor);
+
+		logger.saveLog(`Chat Server -> ${message}`, "error");
+	}
+}
+
+export default AppError;
