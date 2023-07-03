d850414b43fe10d8400b97ebead8f5a5f9439c77
LordPrinz <plazinski2902@gmail.com>
2023-07-03 15:43:15 +0200

    Rewrite to clean code

--- src/server/controllers/messagesController.ts
+++ src/server/controllers/messagesController.ts
@@ -3,146 +3,100 @@ import MessageModel from "../../bot/models/messageModel";
 import catchAsync from "../../utils/server/catchAsync";
 import factory from "./handleFactory";
 import client from "../../bot";
-import { Collection, Message, TextChannel } from "discord.js";
+import { Message, TextChannel } from "discord.js";
 import { formMessage } from "../../utils/commands/dmChatListener";
 import { TMessage } from "../../types/TMessage";
 import AppError from "../../utils/server/AppError";
+import {
+	sendEmptyMessages,
+	sendWrongIdError,
+} from "../../utils/server/validateId";
 
-// TODO: Clean code
 const getServerMessages = () =>
 	catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 		const serverId = req.params.serverId;
 		const channelId = req.params.channelId;
-		const { limit, page } = req.query;
+		const { limit, page, beforeId } = req.query;
 
 		const server = client.guilds.cache.find((g) => g.id === serverId);
 
 		if (!server) {
-			return res.status(422).json({
-				status: "fail",
-				results: 0,
-				data: "Wrong ID",
-			});
+			return sendWrongIdError(res);
 		}
 
-		const channel = server?.channels.cache.find(
+		const channel = server.channels.cache.find(
 			(ch) => ch.id === channelId
-		) as TextChannel;
+		) as TextChannel | null;
 
 		if (!channel) {
-			return res.status(422).json({
-				status: "fail",
-				results: 0,
-				data: "Wrong ID",
-			});
+			return sendWrongIdError(res);
 		}
 
 		const messagesPerPage = limit ? parseInt(limit as string) : 20;
 		const currentPage = page ? parseInt(page as string) : 1;
 
-		let fetchedMessages: Collection<string, Message> | null;
+		let paginatedMessages: Message[];
 
 		const messageNumber = currentPage * messagesPerPage;
 
-		const beforeId = req.query?.beforeId as string | undefined;
-
 		if (beforeId) {
-			let limitation = parseInt(limit as string);
-			if (parseInt(limit as string) > 100) {
-				limitation = 100;
-			}
+			const limitation = parseInt(limit as string);
+			const fetchLimit = limitation > 100 ? 100 : limitation;
 
-			fetchedMessages = await channel.messages.fetch({
-				limit: limitation,
-				before: beforeId,
+			const fetchedMessages = await channel.messages.fetch({
+				limit: fetchLimit,
+				before: beforeId as string,
 			});
 
-			const response: TMessage[] = [];
-
-			fetchedMessages.forEach((msg) => {
-				const message = formMessage(msg);
-
-				response.push(message as any as TMessage);
-			});
-
-			return res.status(200).json({
-				status: "success",
-				results: response.length,
-				data: response,
-			});
-		}
-
-		if (messageNumber <= 100) {
-			fetchedMessages = await channel.messages.fetch({
+			paginatedMessages = Array.from(fetchedMessages.values());
+		} else if (messageNumber <= 100) {
+			const fetchedMessages = await channel.messages.fetch({
 				limit: messageNumber,
 			});
 
 			const startIndex = (currentPage - 1) * messagesPerPage;
 			const endIndex = startIndex + messagesPerPage;
 
-			const paginatedMessages = Array.from(fetchedMessages.values()).slice(
+			paginatedMessages = Array.from(fetchedMessages.values()).slice(
 				startIndex,
 				endIndex
 			);
+		} else {
+			let lastMessageId: string | undefined;
+			let messages;
 
-			const response: TMessage[] = [];
-
-			paginatedMessages.forEach((msg) => {
-				const message = formMessage(msg);
-
-				response.push(message as any as TMessage);
-			});
-
-			return res.status(200).json({
-				status: "success",
-				results: response.length,
-				data: response,
-			});
-		}
-
-		let lastMessageId: string | undefined = undefined;
-
-		let messages = null;
-
-		for (let i = 0; i < messageNumber / 100; i++) {
-			messages = await channel.messages.fetch({
-				before: lastMessageId,
-				limit: 100,
-			});
-
-			lastMessageId = messages.last()?.id;
-
-			if (!lastMessageId) {
-				return res.status(200).json({
-					status: "success",
-					results: 0,
-					data: [],
+			for (let i = 0; i < messageNumber / 100; i++) {
+				messages = await channel.messages.fetch({
+					before: lastMessageId,
+					limit: 100,
 				});
+
+				lastMessageId = messages.last()?.id;
+
+				if (!lastMessageId) {
+					return sendEmptyMessages(res);
+				}
 			}
+
+			if (!messages) {
+				return sendEmptyMessages(res);
+			}
+
+			const messagesLength = messages.size;
+
+			const limitation = req.query.limit as any as number;
+
+			const endIndex =
+				Math.floor(currentPage % (messagesLength / limitation)) === 0
+					? messagesLength
+					: Math.floor(currentPage % (messagesLength / limitation)) * limitation;
+			const startIndex = endIndex - limitation;
+
+			paginatedMessages = Array.from(messages.values()).slice(
+				startIndex,
+				endIndex
+			);
 		}
-
-		if (!messages) {
-			return res.status(200).json({
-				status: "success",
-				results: 0,
-				data: [],
-			});
-		}
-
-		const messagesLength = messages.size;
-
-		const limitation = req.query.limit as any as number;
-
-		const endIndex =
-			Math.floor(currentPage % (messagesLength / limitation)) === 0
-				? messagesLength
-				: Math.floor(currentPage % (messagesLength / limitation)) * limitation;
-		const startIndex = endIndex - limitation;
-
-		const paginatedMessages = Array.from(messages.values()).slice(
-			startIndex,
-			endIndex
-		);
 
 		const response: TMessage[] = [];
 
--- src/utils/server/validateId.ts
+++ src/utils/server/validateId.ts
@@ -1,1 +1,18 @@
+import { Response } from "express";
+
 export default (id: string) => id.match(/^[0-9a-fA-F]{18}$/);
+export const sendWrongIdError = (res: Response) => {
+	res.status(422).json({
+		status: "fail",
+		results: 0,
+		data: "Wrong ID",
+	});
+};
+
+export const sendEmptyMessages = (res: Response) => {
+	res.status(200).json({
+		status: "success",
+		results: 0,
+		data: [],
+	});
+};
