bceb4f58c9aefa95b3d105d1c2f5f41e9fd50436
LordPrinz <plazinski2902@gmail.com>
2024-05-12 01:20:25 +0200

    Add fun command

---
+++ src/commands/(fun)/czy_wiedział.ts
@@ -0,0 +1,36 @@
+import { getRandom } from "@/utils";
+import { CommandObject, CommandType } from "wokcommands";
+
+export default {
+	description: "Answers the damn old question",
+	type: CommandType.BOTH,
+
+	callback: async ({ interaction, message }) => {
+		const randomNumber = getRandom(0, 2);
+
+		let replyMessage = "";
+
+		if (randomNumber === 0) {
+			replyMessage = "Wiedział!";
+		} else {
+			replyMessage = "Nie wiedział sadge bruh";
+		}
+
+		if (randomNumber !== 0) {
+			return {
+				content: replyMessage,
+			};
+		}
+
+		if (interaction) {
+			interaction.channel.send(replyMessage).then((msg) => {
+				msg.react("🙋‍♂️");
+			});
+
+			return ":flag_de:";
+		}
+
+		message.react("🙋‍♂️");
+		return replyMessage;
+	},
+} as CommandObject;
