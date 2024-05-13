d4dc3877edbc38dffd61a06899a41316f5e3173e
LordPrinz <plazinski2902@gmail.com>
2024-05-13 23:34:09 +0200

    Create partyAreaModel

---
+++ src/models/partyAreaModel.ts
@@ -0,0 +1,64 @@
+import mongoose, { Schema } from "mongoose";
+
+type PartyArea = {
+	_id: string; // Guild ID
+	groupId: string;
+	splitChannelId: string;
+	newChannelName: string;
+	commandsChannel?: string;
+};
+
+export type PartyAreaData = Omit<PartyArea, "_id"> & { id: string };
+
+const partyAreaSchema = new Schema<PartyArea>(
+	{
+		_id: {
+			type: String,
+			required: true,
+		},
+		groupId: {
+			type: String,
+			required: true,
+		},
+		splitChannelId: {
+			type: String,
+			required: true,
+		},
+		newChannelName: {
+			type: String,
+			required: true,
+		},
+		commandsChannel: {
+			type: String,
+			required: false,
+		},
+	},
+	{
+		versionKey: false,
+		_id: false,
+	}
+);
+
+partyAreaSchema.set("toJSON", {
+	transform: (_, ret) => {
+		ret.id = ret._id;
+		delete ret._id;
+	},
+});
+
+partyAreaSchema.set("toObject", {
+	transform: (_, ret) => {
+		ret.id = ret._id;
+		delete ret._id;
+	},
+});
+
+let PartyAreaModel: mongoose.Model<PartyArea>;
+
+try {
+	PartyAreaModel = mongoose.model("partyAreas");
+} catch {
+	PartyAreaModel = mongoose.model("partyAreas", partyAreaSchema);
+}
+
+export default PartyAreaModel;
