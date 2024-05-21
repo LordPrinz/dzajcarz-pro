f70fc233492a183fdc4fc8482c708e66187a4353
LordPrinz <plazinski2902@gmail.com>
2024-05-21 19:56:36 +0200

    Add modal component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 import { ActionRow } from "./ActionRow";
 import { TextInput } from "./TextInput";
-
-export { ActionRow, TextInput };
+import { Modal } from "./Modal";
+export { ActionRow, TextInput, Modal };
