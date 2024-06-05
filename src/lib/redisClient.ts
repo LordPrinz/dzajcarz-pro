8764fcec846712333d570bcdfee946c5151e4cc9
LordPrinz <plazinski2902@gmail.com>
2024-06-05 17:11:56 +0200

    Add redis client setup

--- src/lib/redisClient.ts
+++ src/lib/redisClient.ts
@@ -1,1 +1,11 @@
 import redis from "redis";
+
+export const redisClient = redis.createClient();
+
+redisClient.on("error", (error) => {
+	console.error(error);
+});
+
+redisClient.on("connect", () => {
+	console.log("Connected to Redis");
+});
