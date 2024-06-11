e6cef6f8764e15ed03c9818aba0b957df4f30d30
LordPrinz <plazinski2902@gmail.com>
2024-06-11 11:10:46 +0200

    Add message delete  functionality

--- src/events/messageCreate/command-channel.ts
+++ src/events/messageCreate/command-channel.ts
@@ -1,1 +1,20 @@
-export default () => {};
+import { getElements } from "@/helpers/redis/set";
+import { PartyAreaData } from "@/models/partyAreaModel";
+import { sleep } from "@/utils";
+import { type Message } from "discord.js";
+
+export default async (message: Message) => {
+	const partyData = await getElements<PartyAreaData[]>("partyAreas");
+
+	const isCommandChannel = partyData.some(
+		(party) => party.commandsChannel === message.channel.id
+	);
+
+	if (!isCommandChannel) return;
+
+	await sleep(1000 * 16);
+
+	if (message.deletable) {
+		await message.delete();
+	}
+};
