8d1943745316a928dd3b9c6a34080dd332c57738
LordPrinz <plazinski2902@gmail.com>
2024-08-02 22:47:54 +0200

    Add block command

---
+++ src/commands/[party]/block.ts
@@ -0,0 +1,44 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import { type CommandObject, CommandType } from "wokcommands";
+
+export default {
+	description: "Blocks user from your party channel",
+	type: CommandType.BOTH,
+	guildOnly: true,
+	minArgs: 1,
+	expectedArgs: "<user>",
+	options: [
+		{
+			name: "user",
+			description: "User to block",
+			required: true,
+			type: ApplicationCommandOptionType.Mentionable,
+		},
+	],
+	callback: async ({ args, member }) => {
+		if (!args.length) {
+			return { content: "Please mention a user to block", ephemeral: true };
+		}
+
+		if (!member?.voice.channel) {
+			return {
+				content: "You need to be in a voice channel to block a user",
+				ephemeral: true,
+			};
+		}
+
+		const user = args[0];
+
+		const channel = member.voice.channel;
+
+		channel.permissionOverwrites.edit(user, {
+			Connect: false,
+			ViewChannel: false,
+		});
+
+		return {
+			content: `Blocked ${user} from your party channel`,
+			ephemeral: true,
+		};
+	},
+} as CommandObject;
