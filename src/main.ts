469b7ad681c9c6ab9d77ecc20c8744086cce27e7
LordPrinz <plazinski2902@gmail.com>
2025-01-26 12:32:27 +0100

    [DZAJ-4] Add support for production

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { dzajcarz } from './app';
+import { logger } from './core/logger';
 
-// logger.start();
+logger.start();
 
 dzajcarz.handleLogin();
+
+if (process.env.NODE_ENV === 'production') {
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  });
+
+  process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception thrown', error);
+  });
+}
